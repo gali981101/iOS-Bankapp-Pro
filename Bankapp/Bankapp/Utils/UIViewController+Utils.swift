5e190338dabf11a6cd3f1c6ad0b8c5046641518b
Gali <good746018@gmail.com>
2023-06-23 15:46:29 +0800

    feat: Add tab bar

---
+++ Bankapp/Bankapp/Utils/UIViewController+Utils.swift
@@ -0,0 +1,27 @@
+//
+//  UIViewController+Utils.swift
+//  Bankapp
+//
+//  Created by Terry Jason on 2023/6/23.
+//
+
+import UIKit
+
+extension UIViewController {
+    func setStatusBar() {
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.configureWithTransparentBackground() // to hide Navigation Bar Line also
+        navBarAppearance.backgroundColor = appColor
+        UINavigationBar.appearance().standardAppearance = navBarAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearance
+    }
+
+    
+    func setTabBarImage(imageName: String, title: String) {
+        let configuration = UIImage.SymbolConfiguration(scale: .large)
+        let image = UIImage(systemName: imageName, withConfiguration: configuration)
+        tabBarItem = UITabBarItem(title: title, image: image, tag: 0)
+    }
+}
+
+
