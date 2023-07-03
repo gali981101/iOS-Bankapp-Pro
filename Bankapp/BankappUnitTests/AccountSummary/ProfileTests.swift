7b5ac83624a903f39aa1f90455a0565d056b220f
Gali <good746018@gmail.com>
2023-07-03 15:33:31 +0800

    test:

---
+++ Bankapp/BankappUnitTests/AccountSummary/ProfileTests.swift
@@ -0,0 +1,35 @@
+//
+//  ProfileTests.swift
+//  BankappUnitTests
+//
+//  Created by Terry Jason on 2023/7/3.
+//
+
+import Foundation
+import XCTest
+
+@testable import Bankapp
+
+class ProfileTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    func testCanParse() throws {
+        let json = """
+        {
+        "id": "1",
+        "first_name": "Kevin",
+        "last_name": "Flynn",
+        }
+        """
+        
+        let data = json.data(using: .utf8)!
+        let result = try! JSONDecoder().decode(Profile.self, from: data)
+        
+        XCTAssertEqual(result.id, "1")
+        XCTAssertEqual(result.firstName, "Kevin")
+        XCTAssertEqual(result.lastName, "Flynn")
+    }
+}
